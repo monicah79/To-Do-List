a090b46bdff35bfbf2483c7c85c95b7d39e8f910
monicah79 <monniewanjema@gmail.com>
2022-12-18 01:38:23 +0300

    Complete interactive list

--- src/index.js
+++ src/index.js
@@ -38,6 +38,33 @@ const changeLocalStorage = (task, status, index) => {
   localStorage.setItem('todos', JSON.stringify(taskArr));
 };
 
+// reset index
+const resetIndex = () => {
+  const taskArr = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [];
+  const arr = [];
+
+  taskArr.forEach((obj) => {
+    const newObj = { ...obj, index: (arr.length + 1).toString() };
+    arr.push(newObj);
+  });
+  localStorage.setItem('todos', JSON.stringify(arr));
+  window.location.reload();
+};
+
+// update tasks
+const clearCompletedTodo = () => {
+  let taskArr = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [];
+  taskArr = taskArr.filter((todo) => {
+    if (todo.completed !== true) {
+      return true;
+    }
+    return false;
+  });
+  localStorage.setItem('todos', JSON.stringify(taskArr));
+  resetIndex();
+  window.location.reload();
+};
+
 const addTodo = (e) => {
   e.preventDefault();
   const taskArr = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [];
@@ -52,7 +79,7 @@ const addTodo = (e) => {
     attr.value = index;
     li.setAttributeNode(attr);
     li.innerHTML = `<div class="check">
-    <input type="checkbox">
+    <input type="checkbox" class="checkbox">
     <div class="parent"><p class="para">${task}</p></div>
 </div>
 <div>
@@ -68,6 +95,40 @@ const addTodo = (e) => {
     saveToLocalStorage(task, completed, index);
 
     input.value = '';
+
+    // checkbox
+    const checkboxes = li.querySelectorAll('.checkbox');
+    checkboxes.forEach((checkbox) => {
+      checkbox.addEventListener('change', (e) => {
+        if (checkbox.checked === true) {
+          const todo = e.target.nextElementSibling.childNodes[0];
+          todo.style.textDecoration = 'line-through';
+          const todoLi = e.target.parentElement.parentElement;
+          const { index } = todoLi.dataset;
+          const taskArr = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [];
+          taskArr.filter((todo) => {
+            if (todo.index === index) {
+              todo.completed = true;
+            }
+            return false;
+          });
+          localStorage.setItem('todos', JSON.stringify(taskArr));
+        } else {
+          const todo = e.target.nextElementSibling.childNodes[0];
+          todo.style.textDecoration = 'none';
+          const todoLi = e.target.parentElement.parentElement;
+          const { index } = todoLi.dataset;
+          const taskArr = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [];
+          taskArr.filter((todo) => {
+            if (todo.index === index) {
+              todo.completed = false;
+            }
+            return false;
+          });
+          localStorage.setItem('todos', JSON.stringify(taskArr));
+        }
+      });
+    });
 
     // delete task
     const optionBtn = li.querySelector('.fa-ellipsis-v');
@@ -99,21 +160,111 @@ const addTodo = (e) => {
         changeLocalStorage(edit.textContent, completed, editIndex);
       });
     });
-    trash.addEventListener('click', () => {
+    trash.addEventListener('click', (e) => {
       const task = e.target.parentElement.parentElement;
-
       const { index } = task.dataset;
-
       ul.removeChild(li);
-      //   if (ul.children.length === 0) {
-      //     listContainer.classList.remove('show-container');
-      //   }
       delFromLocalStorage(index);
     });
   }
 };
 
 form.addEventListener('submit', addTodo);
+
+const clearBtn = document.querySelector('.bottom');
+clearBtn.addEventListener('click', clearCompletedTodo);
+
+window.addEventListener('DOMContentLoaded', () => {
+  const taskArr = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [];
+  if (taskArr.length > 0) {
+    taskArr.forEach((todo) => {
+      
+      const li = document.createElement('li');
+      li.className = 'li-todo';
+      const attr = document.createAttribute('data-index');
+      attr.value = todo.index;
+      li.setAttributeNode(attr);
+      li.innerHTML = `<div class="check">
+    <input type="checkbox" class="checkbox">
+    <div class="parent"><p class="para">${todo.task}</p></div>
+</div>
+<div>
+    <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+    <i class="fa fa-trash" aria-hidden="true"></i>
+</div>`;
+      const checkboxes = li.querySelectorAll('.checkbox');
+      checkboxes.forEach((checkbox) => {
+        checkbox.addEventListener('change', (e) => {
+          if (checkbox.checked === true) {
+            const todo = e.target.nextElementSibling.childNodes[0];
+            todo.style.textDecoration = 'line-through';
+            const todoLi = e.target.parentElement.parentElement;
+            const { index } = todoLi.dataset;
+            const taskArr = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [];
+            taskArr.filter((todo) => {
+              if (todo.index === index) {
+                todo.completed = true;
+              }
+              return false;
+            });
+            localStorage.setItem('todos', JSON.stringify(taskArr));
+          } else {
+            const todo = e.target.nextElementSibling.childNodes[0];
+            todo.style.textDecoration = 'none';
+            const todoLi = e.target.parentElement.parentElement;
+            const { index } = todoLi.dataset;
+            const taskArr = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [];
+            taskArr.filter((todo) => {
+              if (todo.index === index) {
+                todo.completed = false;
+              }
+              return false;
+            });
+            localStorage.setItem('todos', JSON.stringify(taskArr));
+          }
+        });
+      });
+
+      const optionBtn = li.querySelector('.fa-ellipsis-v');
+      const trash = li.querySelector('.fa-trash');
+      optionBtn.addEventListener('click', (e) => {
+        optionBtn.classList.add('hide');
+        trash.classList.add('show');
+        const edit = e.target.parentElement.previousElementSibling.lastElementChild.childNodes[0];
+        const editInput = document.createElement('input');
+        editInput.type = 'text';
+        editInput.className = 'changed-task';
+        li.classList.add('edit-task');
+        editInput.value = edit.textContent;
+        const editIndex = e.target.parentElement.parentElement.dataset.index;
+        const parentDiv = e.target.parentElement.previousElementSibling.lastElementChild;
+        parentDiv.removeChild(edit);
+        parentDiv.appendChild(editInput);
+        const completed = false;
+        editInput.addEventListener('keypress', (e) => {
+          if (e.key === 'Enter') {
+            edit.textContent = editInput.value;
+
+            parentDiv.appendChild(edit);
+            parentDiv.removeChild(editInput);
+            li.classList.remove('edit-task');
+            optionBtn.classList.remove('hide');
+            trash.classList.remove('show');
+          }
+          changeLocalStorage(edit.textContent, completed, editIndex);
+        });
+      });
+      trash.addEventListener('click', (e) => {
+        const task = e.target.parentElement.parentElement;
+        const { index } = task.dataset;
+        ul.removeChild(li);
+        delFromLocalStorage(index);
+        resetIndex();
+        window.location.reload();
+      });
+    });
+  }
+});
 
 function createNewListItem(task) {
   const li = document.createElement('li');
